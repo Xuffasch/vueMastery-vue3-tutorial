0ace7b068028e917909b8afa5f3fc429df667880
Xuffasch <tristan.lucas@gmail.com>
2021-03-01 11:57:54 +0100

    End of lesson 10 Communicate events + challenge

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -3,6 +3,10 @@ app.component('product-display', {
     premium: {
       type: Boolean,
       required: true
+    },
+    empty: {
+      type: Boolean,
+      required: true
     }
   },
   template: 
@@ -39,6 +43,14 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <button 
+          class="button"
+          style ="{ color: black }"
+          :class="{disabledButton: empty}"
+          v-on:click="removeFromCart">
+          Remove from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -50,16 +62,19 @@ app.component('product-display', {
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 10 },
         ]
     }
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id )
       }
   },
   computed: {
--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+            console.log('cart after addtion : ', this.cart);
+        },
+        removeFromCart(id) {
+            console.log('received id : ', id);
+            if (this.cart.length > 0 && this.cart.indexOf(id) != -1) {
+                console.log("removing item");
+                this.cart.splice(this.cart.indexOf(id), 1)
+                console.log('cart after remove : ', this.cart)
+            }
+        }
+    },
+    computed: {
+        emptyCart() {
+            return this.cart.length ? false : true
+        }
+    }
 })
